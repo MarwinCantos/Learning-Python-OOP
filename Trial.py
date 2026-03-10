b52d3fb0568666c2762804a99918b26b17875633
Marwin Cantos <130458593+MarwinCantos@users.noreply.github.com>
2026-03-10 21:42:55 +0800

    Trial Push

--- Trial.py
+++ Trial.py
@@ -7,13 +7,7 @@ class Info:
         print(f"Name: {self.name}, Age: {self.age}")
 
 user1 = Info("Alice", 30)
-user2 = Info("Bob", 25)
-user3= Info("Charlie", 25)
-user4 = Info("David", 30)
-
 
 print(f"My Name is {user1.name} and I am {user1.age} years old.")
-print(f"My Name is {user2.name} and I am {user2.age} years old.")
-print(f"My Name is {user3.name} and I am {user3.age} years old.")
-print(f"My Name is {user4.name} and I am {user4.age} years old.")
+
 
