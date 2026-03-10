e75e76588d0c674939a11fcf71dc530503dea877
Marwin Cantos <130458593+MarwinCantos@users.noreply.github.com>
2026-03-10 21:32:28 +0800

    Add files via upload

---
+++ Trial.py
@@ -0,0 +1,13 @@
+class Info:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def display(self):
+        print(f"Name: {self.name}, Age: {self.age}")
+
+user1 = Info("Alice", 30)
+user2 = Info("Bob", 25)
+
+print(f"My Name is {user1.name} and I am {user1.age} years old.")
+print(f"My Name is {user2.name} and I am {user2.age} years old.")
